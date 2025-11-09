3f34a91834915d6a8eab514d3eb5923091562678
Prudence ASSOGBA <jprud67@gmail.com>
2025-11-09 11:57:54 +0100

    amélioration de login

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -41,7 +41,7 @@ class DefaultController extends AbstractController
         ]);
     }
 
-    #[Route('/logout', name: 'logout')]
+    #[Route('/logout', name: 'app_logout')]
     public function logout(): void
     {
         throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+#[Route('/{_locale}', requirements: ['_locale' => '[a-z]{2}'])]
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
@@ -24,16 +25,4 @@ class SecurityController extends AbstractController
         return $this->render('frontend/security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-    #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
-    {
-        // This method can be blank - it will be intercepted by the logout key on your firewall.
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    }
-
-    #[Route(path: '/user-dashboard', name: 'user_dashboard')]
-    public function userDashboard(): Response
-    {
-        return $this->redirectToRoute('admin_dashboard');
-    }
 }
